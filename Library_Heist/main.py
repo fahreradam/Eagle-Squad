76cefdad7b9cad748614bee8d153bff5fdd8d322
fahreradam <72369125+fahreradam@users.noreply.github.com>
2021-02-18 13:04:13 -0500

    Movement of the Player class

--- Library_Heist/main.py
+++ Library_Heist/main.py
@@ -1,17 +1,21 @@
 import pygame
+import player
 pygame.init()
 
 win_w = 800
 win_h = 600
 win = pygame.display.set_mode((win_w, win_h))
-
+player = player.Player(400, 300, win)
 clock = pygame.time.Clock()
-done = False
 
+done = False
 while not done:
     delta_time = clock.tick() / 1000
     event = pygame.event.poll()
     keys = pygame.key.get_pressed()
+
+    player.draw()
+    player.move(delta_time)
 
     # Exiting
     if event.type == pygame.KEYDOWN:
---
+++ Library_Heist/player.py
@@ -0,0 +1,22 @@
+import pygame
+
+class Player:
+    def __init__(self, x, y, surf):
+        self.position = [x, y]
+        self.img =
+        self.win = surf
+
+    def draw(self):
+        self.win.blit(self.img, self.position)
+        pygame.display.flip()
+
+    def move(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.position[0] -= 50 * dt
+        if keys[pygame.K_d]:
+            self.position[0] += 50 * dt
+        if keys[pygame.K_w]:
+            self.position[1] -= 50 * dt
+        if keys[pygame.K_s]:
+            self.position[1] += 50 * dt
