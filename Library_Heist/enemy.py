7e8ccba53f55d405aaf89e6dcf95913b3757db50
WiseWizard101 <thomasberryhill93@yahoo.com>
2021-03-04 03:32:04 -0500

    Enemy AI
    It works, wow

--- Library_Heist/enemy.py
+++ Library_Heist/enemy.py
@@ -5,13 +5,20 @@ class Enemy(Player):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.position = [x, y]
-
+        self.timer = 100
+        self.random_direction1 = [-100, 100, 0]
+        self.random_direction2 = [-100, 100, 0]
+        self.dir1 = 0
+        self.dir2 = 0
 
     def movement(self, dt):
-        if self.position[0] >= 0:
-            self.random_x = random.randint(-100, 100)
-            self.random_y = random.randint(-100, 100)
-            # self.position[0] += 100 * dt
-            self.position[0] += self.random_x * dt
-            self.position[1] += self.random_y * dt
+        if self.timer > 0:
+            self.position[0] -= self.dir1 * dt
+            self.position[1] -= self.dir2 * dt
+            self.timer -= 1
+        elif self.timer == 0:
+            self.dir1 = random.choice(self.random_direction1)
+            self.dir2 = random.choice(self.random_direction2)
+            self.timer = 200
+            print(self.dir1)
 
